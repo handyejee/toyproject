60f2d90566cdfce1db0d9bd6853b5842a4bb051f
손예지 <yjson@daou.co.kr>
2023-04-17 08:50:55 +0900

    화면 추가

---
+++ board/mySQL/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <form action="process_create.php" method="POST">
+        <p></p>
+</body>
+</html>
