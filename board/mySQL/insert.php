ba0c7fdd8e1ca3017a77bba01a79c1ef72f0830b
손예지 <yjson@daou.co.kr>
2023-04-17 08:51:26 +0900

    error msg 추가

--- board/mySQL/insert.php
+++ board/mySQL/insert.php
@@ -1,13 +1,18 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "root", "livDB");
-mysqli_query($conn, "
+$sql = " 
     INSERT INTO topic
-        (title, description, created)
-        VALUES(
-            'MySQL',
-            'MySQL is..',
-            NOW()
-        )
-");
-
+        (title, 
+        description, 
+        created
+        ) VALUES (
+        'MySQL',
+        'MySQL is..',
+        NOW()
+    )";
+mysqli_query($conn, $sql);
+if($result === false){
+    echo mysqli_error($conn); // error 가 어떤 error 인지 database에서 알려줌
+}
+    
 ?>
