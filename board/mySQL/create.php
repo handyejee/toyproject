8ffa1b71e5b8827870d5b2e0503a60729715d206
yjson <handyejee14@gmail.com>
2023-04-23 21:48:35 +0900

    data 입력 페이지 생성

---
+++ board/mySQL/create.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB</title>
+</head>
+
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <form action="process_create.php" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p><textarea name="description" placeholder="description"></textarea></p>
+        <p><input type="submit"></p>
+</body>
+
+</html>
