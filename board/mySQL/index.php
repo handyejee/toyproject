e751e4ac65c1c473b24d27e1089d0c826f6666bd
yjson <handyejee14@gmail.com>
2023-04-23 21:49:43 +0900

    main page- 입력 부분 삭제

--- board/mySQL/index.php
+++ board/mySQL/index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB</title>
 </head>
+
 <body>
     <h1>WEB</h1>
     <ol>
         <li>HTML</li>
     </ol>
-    <form action="process_create.php" method="POST">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p><textarea name="description" placeholder="description"></textarea></p>
-        <p><input type="submit"></p>
-    </body>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>
+    test page with php
+</body>
+
 </html>
