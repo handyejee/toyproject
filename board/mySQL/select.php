ecf3074442e851e5c418c32eaa62cb1682a7bc86
yjson <handyejee14@gmail.com>
2023-04-25 23:49:50 +0900

    query syntax 수정

--- board/mySQL/select.php
+++ board/mySQL/select.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect("localhost", "root", "root", "livDB"); //mysql 접속
 
 echo "<h1>single row</h1>";
-$sql = "SELECT * FROM topic WHERE id = 6ㄴ"; 
+$sql = "SELECT * FROM topic WHERE id = 6"; 
 // $sql = "SELECT * FROM topic 이렇게만 해서 조회하면 전체가 다 조회되기 때문에 데이터가 많을 경우에 비추
 // WHERE id = 19 -> id값이 19 인 행을 가져온다 
 // DELETE 해도 id 값은 초기화 되지 않나? -> Unique id 이기 때문에 Delete 이후에도 Index는 돌아오지 않는다.
