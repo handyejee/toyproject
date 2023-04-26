45a60f7a112e9cb11e20b166dbc86bfae37975f4
yjson <handyejee14@gmail.com>
2023-04-26 23:10:40 +0900

    반복문으로 데이터베이스에서 값 가져오기

--- board/mySQL/select.php
+++ board/mySQL/select.php
@@ -1,8 +1,8 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "root", "livDB"); //mysql 접속
 
-echo "<h1>single row</h1>";
-$sql = "SELECT * FROM topic WHERE id = 6"; 
+// echo "<h1>single row</h1>";
+$sql = "SELECT * FROM topic"; 
 // $sql = "SELECT * FROM topic 이렇게만 해서 조회하면 전체가 다 조회되기 때문에 데이터가 많을 경우에 비추
 // WHERE id = 19 -> id값이 19 인 행을 가져온다 
 // DELETE 해도 id 값은 초기화 되지 않나? -> Unique id 이기 때문에 Delete 이후에도 Index는 돌아오지 않는다.
@@ -13,33 +13,14 @@ $result = mysqli_query($conn, $sql);
 // var_dump($result->num_rows); 
 
 $row = mysqli_fetch_array($result);
+// echo '<h1>'.$row['title'].'<h1>'; // 연관배열
+// echo $row['description'];
 
-echo '<h1>'.$row['title'].'<h1>'; // 연관배열
-echo $row['description'];
+// echo"\$row : <pre>";
+// print_r($row);
+// echo "</pre>";
 
-//multiple row
-echo "<h1>multi row</h1>";
-$sql = "SELECT * FROM topic"; 
-$result = mysqli_query($conn, $sql);
-
-$row = mysqli_fetch_array($result);
-echo '<h2>'.$row['title'].'<h2>'; // 연관배열
-echo $row['description'];
-
-$row = mysqli_fetch_array($result);
-echo '<h2>'.$row['title'].'<h2>'; // 연관배열
-echo $row['description'];
-
-$row = mysqli_fetch_array($result);
-echo '<h2>'.$row['title'].'<h2>'; // 연관배열
-echo $row['description'];
-
-$row = mysqli_fetch_array($result);
-echo '<h2>'.$row['title'].'<h2>'; // 연관배열
-echo $row['description'];
-
-
-// 데이터가 있는 만큼만 노출하게
+// 데이터가 있는 만큼만 노출하게 반복문 사용
 while($row = mysqli_fetch_array($result)) {
     echo '<h2>'.$row['title'].'<h2>'; // 연관배열
     echo $row['description'];
