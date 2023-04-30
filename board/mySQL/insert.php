7a741587f1c26d190708d7ffc77583a5135346b7
yjson <handyejee14@gmail.com>
2023-05-01 07:13:41 +0900

    query 전달 안되도록 주석처리

--- board/mySQL/insert.php
+++ board/mySQL/insert.php
@@ -1,16 +1,16 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "root", "livDB");
-$sql = " 
-    INSERT INTO topic
-        (title, 
-        description, 
-        created
-        ) 
-        VALUES (
-        'MySQL',
-        'MySQL is..',
-        NOW()
-    )";
+// $sql = " 
+//     INSERT INTO topic
+//         (title, 
+//         description, 
+//         created
+//         ) 
+//         VALUES (
+//         'MySQL',
+//         'MySQL is..',
+//         NOW()
+//     )";
 
 mysqli_query($conn, $sql);
 if($result === false){
