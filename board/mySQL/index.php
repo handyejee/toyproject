1882882b336c81424e00044a5d87905ba1777b1a
yjson <handyejee14@gmail.com>
2023-05-01 07:18:22 +0900

    query가져와서 화면 보여주는 부분 추가

--- board/mySQL/index.php
+++ board/mySQL/index.php
@@ -2,7 +2,31 @@
 $conn = mysqli_connect("localhost", "root", "root", "livDB");
 
 $sql = "SELECT * FROM topic";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql); //database 서버에 전송하는 api
+$list = ''; 
+while ($row = mysqli_fetch_array($result)) {
+    //<li><a href="index.php?id=19">MySQL</a></li>
+    // mysql 내 아이디가 3인 것 부터 db에서 가져온다
+    $list = $list."<li><a 
+    href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    //실제 화면 링크 : index.php?id=4
+}
+
+$article = array(
+    'title'=>'Welcome',
+    'description'=>'Hello WEB!'
+);
+if (isset($_GET['id'])) {
+    $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($conn, $sql); // db에 쿼리 보내는
+    $row = mysqli_fetch_array($result); // 앞에 title 값을 가져왔던거와는 달리 id값을 primary key로 설정했기 때문에 반복할 필요 없다.
+    $article = array(
+        'title'=>$row['title'],
+        'description'=>$row['description']
+    );
+}
+print_r($article);
+
 ?>
 
 <!DOCTYPE html>
@@ -18,11 +42,11 @@ $result = mysqli_query($conn, $sql);
 <body>
     <h1>WEB</h1>
     <ol>
-        <li>HTML</li>
+        <?= $list ?>
     </ol>
     <a href="create.php">create</a>
-    <h2>Welcome</h2>
-    test page with php
+    <h2><?= $article['title'] ?></h2>
+    <?= $article['description'] ?>
 </body>
 
 </html>
