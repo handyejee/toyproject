47ecd3643c3f0919187860c9041e913e34107bd5
yjson <handyejee14@gmail.com>
2023-05-14 22:46:21 +0900

    bootstrap 추가

--- board/board/signUp/173-signUpForm.php
+++ board/board/signUp/173-signUpForm.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>회원가입 페이지</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
 </head>
 
 <body>
@@ -49,7 +50,9 @@
                 <td colspan="2"><input type="submit" value="가입하기" /></td>
             </tr>
         </table>
-
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script> -->
 </body>
 
 </html>
