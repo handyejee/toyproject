9e7ab29e283645fcde00dabb7c449679f212460a
yjson <handyejee14@gmail.com>
2023-05-17 06:52:18 +0900

    로그인 페이지 생성

---
+++ board/board/signIn/175-signInForm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인</title>
+</head>
+
+<body>
+    <h1>로그인</h1>
+    <form name="signIn" method="POST" action="./176-signInProcess.php"></form>
+</body>
+<table>
+    <tr>
+        <td><input type="input" id="userID" placeholder="아이디" required></td>
+    </tr>
+    <tr>
+        <td><input type="password" id="password" placeholder="비밀번호" required></td>
+    </tr>
+    <tr>
+        <td colspan="2"><input type="submit" value="로그인"></td>
+    </tr>
+</table>
+
+</html>
