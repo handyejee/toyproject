1e536f035745a9ade241f4eafa2fd69d5e00b7ff
yjson <handyejee14@gmail.com>
2023-05-18 21:42:07 +0900

    sql error 출력 구문 수정

--- board/mySQL/process_update.php
+++ board/mySQL/process_update.php
@@ -28,7 +28,7 @@ $result = mysqli_multi_query($conn, $sql);
 // mysqli_multi_query : 단 한개의 쿼리만 실행하도록 하는 함수
 if ($result === false) {
     echo "저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의하세요.";
-    error_log(mysqli_error($conn));
+    echo mysqli_error($conn);
     
 } else {
     echo "성공했습니다. <a href='index.php'>돌아가기</a>";
