6df8500d0bc99a95ee9d8c2ae94235bda36ac088
yjson <handyejee14@gmail.com>
2023-05-29 15:30:00 +0900

    post 값 변수로 넣어 가져오도록 변경

--- mySQL/process_update.php
+++ mySQL/process_update.php
@@ -3,15 +3,19 @@ $conn = mysqli_connect('localhost', 'root', 'root', 'livDB');
 
 // var_dump($_POST); // post 로 데이터가 잘 넘어왔는지 확인
 
+$id = $_POST['id'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+
 settype($_POST['id'], 'integer');
 $filterArr = array(
     /* 
     process_create.php 에서 생성할때는 Id 값이없어서 id값에 대한 고려가 필요 없는데
     update 할때는 Url에 ?id= 로 있기 때문에 id 값에 대한 처리도 필요하다!
     */
-    'id'=>mysqli_real_escape_string($conn, $_POST['id']),
-    'title'=>mysqli_real_escape_string($conn, $_POST['title']),
-    'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+    'id'=>mysqli_real_escape_string($conn, $id),
+    'title'=>mysqli_real_escape_string($conn, $title),
+    'description'=>mysqli_real_escape_string($conn, $description)
 ); 
 
 $sql = "
@@ -31,7 +35,7 @@ if ($result === false) {
     echo mysqli_error($conn);
     
 } else {
-    echo "성공했습니다. <a href='index.php'>돌아가기</a>";
+    echo "수정에 성공했습니다. <a href='index.php'>게시판 목록으로 돌아가기</a>";
 }
 
 ?>
